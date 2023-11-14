b1959e657d3a5f1d3d8afdcfdb057a7c6a81fd1a
José Galinha <j.b.galinha@gmail.com>
2023-11-14 17:48:42 +0000

    :construction: WIP Booking Entity
    Signed-off-by: José Galinha <j.b.galinha@gmail.com>

---
+++ Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,53 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public sealed class Booking : Entity {
+    private Booking(Guid id,
+        Guid apartmentId,
+        Guid userId,
+        DateRange duration,
+        Money priceForPeriod,
+        Money cleaningFee,
+        Money amenitiesUpCharge,
+        Money totalPrice,
+        BookingStatus status,
+        DateTime createdOnUtc) : base(id) {
+        ApartmentId = apartmentId;
+        UserId = userId;
+        Duration = duration;
+        PriceForPeriod = priceForPeriod;
+        CleaningFee = cleaningFee;
+        AmenitiesUpCharge = amenitiesUpCharge;
+        TotalPrice = totalPrice;
+        Status = status;
+        CreatedOnUtc = createdOnUtc;
+    }
+
+    public Guid ApartmentId { get; private set; }
+    public Guid UserId { get; private set; }
+    public DateRange Duration { get; private set; }
+    public Money PriceForPeriod { get; private set; }
+    public Money CleaningFee { get; private set; }
+    public Money AmenitiesUpCharge { get; private set; }
+    public Money TotalPrice { get; private set; }
+    public BookingStatus Status { get; private set; }
+    public DateTime CreatedOnUtc { get; private set; }
+    public DateTime? ConfirmedOnUtc { get; }
+    public DateTime? RejectedOnUtc { get; }
+    public DateTime? CompletedOnUtc { get; }
+    public DateTime? CancelledOnUtc { get; }
+
+    public static Booking Reserve(
+        Guid apartmentId,
+        Guid userId,
+        DateRange duration,
+        DateTime utcNow) {
+        var booking = new Booking(
+            Guid.NewGuid(),
+            apartmentId,
+            userId,
+            duration);
+    }
+}
